e32ccf68f8c40f2a675d115a6b287db51127d20a
Burak <burak@90pixel.com>
2025-08-03 11:19:02 +0300

    Enhance BlockWrapper component by adding buttons for adding and dragging blocks; update App.css to apply flex display to block-wrapper; adjust button styles in index.css for improved layout.

--- src/Block/BlockWrapper.tsx
+++ src/Block/BlockWrapper.tsx
@@ -1,7 +1,17 @@
 import type { PropsWithChildren } from "react";
 
 function BlockWrapper({children}:PropsWithChildren) {
-  return <div className="block-wrapper">{children}</div>
+  return (
+    <div className="block-wrapper">
+      <button className="add-block-button" contentEditable={false}>
+        +
+      </button>
+      <button className="drag-handle-button" contentEditable={false}>
+        #
+      </button>
+      {children}
+    </div>
+  );
 }
 
 export default BlockWrapper;
